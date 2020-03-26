5dd9e1a775345bda6d1c87ca9e6c15378e8fbe21
Sierra Morgan <sierramorgan07@gmail.com>
2020-03-26 18:07:17 -0400

    finalized some stuff

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
-const busy_hours = require('busy-hours');
+
 // Setting up the public directory
 app.use(express.static('client'));
 
