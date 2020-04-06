11960bb231e36ea1f8727b08f5fa1ce7e3154657
Sierra Morgan <sierramorgan07@gmail.com>
2020-04-06 17:06:38 -0400

    added qr support

--- client/script.js
+++ client/script.js
@@ -8,26 +8,26 @@ var canvas, ctx, flag = false,
 var x = "black",
     y = 2;
 
-function handleImage(e){
-  canvas = document.getElementById('can');
-  ctx = canvas.getContext("2d");
-  document.getElementById("imageLoader").style.display = "none";
-  var reader = new FileReader();
+function handleImage(e) {
+    canvas = document.getElementById('can');
+    ctx = canvas.getContext("2d");
+    document.getElementById("imageLoader").style.display = "none";
+    var reader = new FileReader();
 
-  reader.onload = function(event){
-    var img = new Image();
-      img.onload = function(){
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx.drawImage(img,0,0);
-      }
-      img.src = event.target.result;
+    reader.onload = function(event) {
+        var img = new Image();
+        img.onload = function() {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        }
+        img.src = event.target.result;
     }
-    reader.readAsDataURL(e.target.files[0]);     
+    reader.readAsDataURL(e.target.files[0]);
 }
 
 function init() {
-  
+
     canvas = document.getElementById('can');
     ctx = canvas.getContext("2d");
     w = canvas.width;
@@ -37,9 +37,9 @@ function init() {
 
     //window.ondragover = function(e) {e.preventDefault()}
     //window.ondrop = function(e) {e.preventDefault(); draw(e.dataTransfer.files[0]); }
-        
+
     // function draw(file){
-        
+
     //     var img =new Image();
     //     // URL @ Mozilla, webkitURL @ Chrome
     //     img.src = (window.webkitURL ? webkitURL : URL).createObjectURL(file);
@@ -72,16 +72,14 @@ function clearCanvas(canvas, ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function download() {
-   document.getElementById("link").innerHTML = "";
-
-   var link = document.getElementById('link');
-        link.innerHTML = '<button>Download Image</button>';
-        link.href = "#";
-        link.download = "littleBits.png";
-        link.className = "link";
-
-    document.link.appendChild(link, canvas);
+function download(canvas) {
+    //document.getElementById("link").innerHTML = "";
+    var url = canvas.toDataURL("image/png");
+    var link = document.getElementById('link');
+    link.innerHTML = '<button>Download Image</button>';
+    link.href = url;
+    link.download = "littleBits.png";
+    link.className = "link";
 }
 
 function color(obj) {
@@ -225,6 +223,15 @@ function isCanvasBlank(canvas) {
     return !pixelBuffer.some(color => color !== 0);
 }
 
+function qrCode(image) {
+    var typeNumber = 40;
+    var errorCorrectionLevel = 'H';
+    var qr = qrcode(typeNumber, errorCorrectionLevel);
+    qr.addData(image);
+    qr.make();
+    document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+}
+
 function convertCanvasToImage(canvas) {
     var image = new Image();
     image.src = canvas.toDataURL("image.png");
@@ -274,117 +281,121 @@ function convertCanvasToImage(canvas) {
         context.drawImage(canvas, 0, 0, scaledWidth, scaledHeight, 0, 0, width, height);
 
         // Append canvas 
-        download();
-        document.getElementById("result").appendChild(canvas);
-      }
+        var newnew = document.getElementById("can");
+        download(newnew);
+
+        var qr = qrCode(image);
+        console.log(qr);
     }
 
-    function imgcap() {
+}
 
-        // The width and height of the captured photo. We will set the
-        // width to the value defined here, but the height will be
-        // calculated based on the aspect ratio of the input stream.
+function imgcap() {
 
-        canvas = document.getElementById("can");
+    // The width and height of the captured photo. We will set the
+    // width to the value defined here, but the height will be
+    // calculated based on the aspect ratio of the input stream.
 
-        var width = 600; // We will scale the photo width to this
-        var height = 0; // This will be computed based on the input stream
+    canvas = document.getElementById("can");
 
-        // |streaming| indicates whether or not we're currently streaming
-        // video from the camera. Obviously, we start at false.
+    var width = 600; // We will scale the photo width to this
+    var height = 0; // This will be computed based on the input stream
 
-        var streaming = false;
+    // |streaming| indicates whether or not we're currently streaming
+    // video from the camera. Obviously, we start at false.
 
-        // The various HTML elements we need to configure or control. These
-        // will be set by the startup() function.
+    var streaming = false;
 
-        var video = null;
-        var canvas = null;
-        var photo = null;
-        var startbutton = null;
+    // The various HTML elements we need to configure or control. These
+    // will be set by the startup() function.
 
-        // Fill the photo with an indication that none has been
-        // captured.
+    var video = null;
+    var canvas = null;
+    var photo = null;
+    var startbutton = null;
 
-        function clearphoto() {
-            var canvas = document.getElementById('can');
-            var context = canvas.getContext('2d');
-            context.fillStyle = "#AAA";
-            context.fillRect(0, 0, canvas.width, canvas.height);
+    // Fill the photo with an indication that none has been
+    // captured.
 
-            //var data = canvas.toDataURL('image/png');
-            //photo.setAttribute('src', data);
+    function clearphoto() {
+        var canvas = document.getElementById('can');
+        var context = canvas.getContext('2d');
+        context.fillStyle = "#AAA";
+        context.fillRect(0, 0, canvas.width, canvas.height);
 
-        }
+        //var data = canvas.toDataURL('image/png');
+        //photo.setAttribute('src', data);
 
-        function startup() {
-            video = document.getElementById('video');
-            video.style.display = "inline-block";
-            canvas = document.getElementById('can');
-            photo = document.getElementById('photo');
-            startbutton = document.getElementById('startbutton');
-            startbutton.style.display = "inline-block";
+    }
 
-            navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-                .then(function(stream) {
-                    video.srcObject = stream;
-                    video.play();
-                })
-                .catch(function(err) {
-                    console.log("An error occurred: " + err);
-                });
+    function startup() {
+        video = document.getElementById('video');
+        video.style.display = "inline-block";
+        canvas = document.getElementById('can');
+        photo = document.getElementById('photo');
+        startbutton = document.getElementById('startbutton');
+        startbutton.style.display = "inline-block";
 
-            video.addEventListener('canplay', function(ev) {
-                if (!streaming) {
-                    height = video.videoHeight / (video.videoWidth / width);
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function(stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function(err) {
+                console.log("An error occurred: " + err);
+            });
 
-                    // Firefox currently has a bug where the height can't be read from
-                    // the video, so we will make assumptions if this happens.
+        video.addEventListener('canplay', function(ev) {
+            if (!streaming) {
+                height = video.videoHeight / (video.videoWidth / width);
 
-                    if (isNaN(height)) {
-                        height = width / (4 / 3);
-                    }
+                // Firefox currently has a bug where the height can't be read from
+                // the video, so we will make assumptions if this happens.
 
-                    video.setAttribute('width', width);
-                    video.setAttribute('height', height);
-                    canvas.setAttribute('width', width);
-                    canvas.setAttribute('height', height);
-                    streaming = true;
+                if (isNaN(height)) {
+                    height = width / (4 / 3);
                 }
-            }, false);
 
-            startbutton.addEventListener('click', function(ev) {
-                takepicture();
-                ev.preventDefault();
-            }, false);
+                video.setAttribute('width', width);
+                video.setAttribute('height', height);
+                canvas.setAttribute('width', width);
+                canvas.setAttribute('height', height);
+                streaming = true;
+            }
+        }, false);
 
+        startbutton.addEventListener('click', function(ev) {
+            takepicture();
+            ev.preventDefault();
+        }, false);
+
+        clearphoto();
+    }
+
+    // Capture a photo by fetching the current contents of the video
+    // and drawing it into a canvas, then converting that to a PNG
+    // format data URL. By drawing it on an offscreen canvas and then
+    // drawing that to the screen, we can change its size and/or apply
+    // other changes before drawing it.
+
+    function takepicture() {
+        var canvas = document.getElementById('can');
+        var context = canvas.getContext('2d');
+        document.getElementById("video").style.display = "none";
+        document.getElementById("startbutton").style.display = "none";
+
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+            console.log("captured");
+
+        } else {
             clearphoto();
         }
-
-        // Capture a photo by fetching the current contents of the video
-        // and drawing it into a canvas, then converting that to a PNG
-        // format data URL. By drawing it on an offscreen canvas and then
-        // drawing that to the screen, we can change its size and/or apply
-        // other changes before drawing it.
-
-        function takepicture() {
-            var canvas = document.getElementById('can');
-            var context = canvas.getContext('2d');
-            document.getElementById("video").style.display = "none";
-            document.getElementById("startbutton").style.display = "none";
-
-            if (width && height) {
-                canvas.width = width;
-                canvas.height = height;
-                context.drawImage(video, 0, 0, width, height);
-                console.log("captured");
-
-            } else {
-                clearphoto();
-            }
-        }
-
-        // Set up our event listener to run the startup process
-        // once loading is complete.
-        startup();
     }
+
+    // Set up our event listener to run the startup process
+    // once loading is complete.
+    startup();
+}
